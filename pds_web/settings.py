ec1c16f9d88e0476dbbd0773aaa1db507ebc50eb
karlaf <karlaf@motorola.com>
2019-05-03 11:27:39 -0400

    teste3

--- pds_web/settings.py
+++ pds_web/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import dj_database_url
+#import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,5 +139,5 @@ os.makedirs(STATIC_ROOT, exist_ok=True)
 APPEND_SLASH=False
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+#prod_db  =  dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(prod_db)
