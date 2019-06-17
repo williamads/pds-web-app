1f371aac6a0cad52cf40756f6ac4c71a59757412
Karla Felix <karlaf@motorola.com>
2019-06-17 17:20:44 -0400

    teste1.1

--- pds_web/urls.py
+++ pds_web/urls.py
@@ -23,7 +23,7 @@ from pds_web.views import z_transform_request
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('/', home),
     path('script', script),
     path('convolution', convolution_request),
     path('z-transform', z_transform_request),
